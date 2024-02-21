e9ae840351bcb1171344ac4abb7abc7c229be281
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-21 16:28:28 +0100

    milestone completed

--- js/script.js
+++ js/script.js
@@ -6,7 +6,21 @@ createApp({
     data() {
         return {
 
-            
+            mails: [],
+
         }
+    },
+    mounted() {
+
+        for(let i = 0; i < 10; i++) {
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+    
+            // ora salvo la mail nei miei data nell'oggetto mail
+            this.mails.push(result.data.response)
+
+        });
+        }
+
     }
 }).mount('#app');
