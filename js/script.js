142d3879712744deca3c20857616f64c4310ec90
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-22 11:57:14 +0100

    exercise completed + experiment

--- js/script.js
+++ js/script.js
@@ -8,9 +8,11 @@ createApp({
 
             mails: [],
 
+            // count: 0,
         }
     },
     mounted() {
+
 
         for(let i = 0; i < 10; i++) {
             
@@ -22,5 +24,35 @@ createApp({
         });
         }
 
+
+// TENTATIVI INUTILI
+
+        //     while (this.count < 3) {
+        //     // for (let i = 0; i < 10; i++) {
+        //         setTimeout(() => {
+
+        //             console.log("ciao")
+        //         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+        
+        //         let risultato = result.data.response;
+        //         let risultatostr = String(risultato)
+
+        //         console.log(risultatostr)
+        //         if (risultatostr.includes("fede")) {
+        //             console.log("*************");
+        //             console.log(risultatostr)
+        //             this.mails.push(risultatostr)
+        //             this.count++
+        //         }
+        //         // ora salvo la mail nei miei data nell'oggetto mail
+        //     //  i = this.count;
+        //     //  console.log(i)
+
+        //    });
+            
+
+        //     }, 1000)
+
+    // }
     }
 }).mount('#app');
